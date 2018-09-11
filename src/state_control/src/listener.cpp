9bac2782d870298d88532464fb58c581298fab8b
Anderson Domingues <ti.andersondomingues@gmail.com>
2018-09-11 17:20:57 -0300

    fixed urdf file for displaying colors correctly. also fixed some link positions to avoid visual glitches on overlaping areas.

--- src/state_control/src/listener.cpp
+++ src/state_control/src/listener.cpp
@@ -1,92 +1,75 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-// %Tag(FULLTEXT)%
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %Tag(CALLBACK)%
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+//state definiton
+#define STATE_IDLE 0 /* when no movement is occuring */
+#define STATE_TUR1 1 /* when turning for the first time */
+#define STATE_TUR2 2 /* when turning for the second time */
+#define STATE_MOVE 3 /* forward movement to the checkpoint */
+
+//state variable (prevents stupid people from sending commands during movement
+char __state = STATE_IDLE;
+
+
+//publisher ptr (needs to be outside since ros::init must come first)
+ros::Publisher* pub_ptr;
+
+//callback when receiving new commands
+void gotoCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-// %EndTag(CALLBACK)%
+	//message to achieve the goal
+	geometry_msgs::Twist m;
+	
+	ROS_INFO("Received command: [%d]", msg->linear.x);
+	switch(__state){
 
+		//do nothing, stay idle until some location is sent by host
+		case STATE_IDLE:
+			//TODO: missing condition to change state
+			break;
+		
+		//turn the bot until it is facing the checkpoint (change angular, turn1)
+		case STATE_TUR1:
+			pub_ptr->publish(msg);
+			//TODO: missing condition to change state
+			break;
+		
+		//move until checkpoint is reached
+		case STATE_MOVE: 
+
+			m.linear.x = 0;
+			m.linear.y = 0;
+			m.linear.z = 0;
+			m.angular.x = 0;
+			m.angular.y = 0;
+			m.angular.z = 0;
+			
+			//TODO: missing condition to change state
+			pub_ptr->publish(m);
+			ROS_INFO("Waiting for movement to finish");
+			break;
+		
+		//turn the bot until it gets to the original orientation (change angular, turn2)
+		case STATE_TUR2:
+			pub_ptr->publish(msg);
+			//TODO: missing condition to change state
+			break;
+	}
+}
+
+//setup
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
   ros::init(argc, argv, "listener");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
+  ros::Publisher  pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  ros::Subscriber sub = n.subscribe("move", 1000, gotoCallback);;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-// %EndTag(SUBSCRIBER)%
+  pub_ptr = &pub;
+  __state = STATE_IDLE;
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-// %Tag(SPIN)%
   ros::spin();
-// %EndTag(SPIN)%
 
   return 0;
 }
