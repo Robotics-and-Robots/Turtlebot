7cbc015541880b2fcb700915f3c532247bda69dc
robo <ti.andersondomingues@gmail.com>
2018-09-25 10:47:29 -0300

    la la land

--- src/kobuki_driver/src/kobuki_driver.cpp
+++ src/kobuki_driver/src/kobuki_driver.cpp
@@ -14,8 +14,8 @@
 /*====================================================================================================*/
 
 /*====================================================================================================*/
-/* Basic serial communication from:  					                                              */
-/* www.xanthium.in										                                              */
+/* Basic serial communication from:  					                              */
+/* www.xanthium.in				                                                      */
 /* Copyright (C) 2014 Rahul.S                                                                         */
 /*====================================================================================================*/
 
@@ -24,7 +24,7 @@
 /* ---------------------------------------------------------------------------------------------------*/ 
 /* 1) Compile the  kobuki_driver.c  file using gcc on the terminal (without quotes)                   */
 /*                                                                                                    */
-/*	" gcc -o kobuki_driver kobuki_driver.c "                                                          */
+/*	" gcc -o kobuki_driver kobuki_driver.c "                                                      */
 /*                                                                                                    */
 /* 2) Linux will not allow you to access the serial port from user space,you have to be root.So use   */
 /*    "sudo" command to execute the compiled binary as super user.                                    */
@@ -44,16 +44,19 @@
 /* use "man termios" to get more info about  termios structure */
 /*-------------------------------------------------------------*/
 
-	#include <cstring>
-	#include <iostream>
-    #include <stdio.h>
-    #include <fcntl.h>   /* File Control Definitions           */
-    #include <termios.h> /* POSIX Terminal Control Definitions */
-    #include <unistd.h>  /* UNIX Standard Definitions 	       */ 
-    #include <errno.h>   /* ERROR Number Definitions           */
-    #include <stdint.h>
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+#include <fcntl.h>   /* File Control Definitions           */
+#include <termios.h> /* POSIX Terminal Control Definitions */
+#include <unistd.h>  /* UNIX Standard Definitions 	       */ 
+#include <errno.h>   /* ERROR Number Definitions           */
+#include <stdint.h>
 
 using namespace std;
+
+#define IN_BUFFER_SIZE 20     /* in buffer has 20 words */
+#define CFG_DELAY 3           /* wait for 3 seconds before recvn' packages */
 
 void SerialConfig(int*);
 void MountPacket_Twist(unsigned char*, unsigned int, int, int, int, int, int, int);
@@ -61,73 +64,71 @@ void MountPacket_Sound(unsigned char*, unsigned int);
 
 int main(void)
 {
-    int             fd;
-    int             written_bytes; 
-    int             read_bytes;
-    unsigned int    packet_size = 13;       // PT
-    // unsigned int    packet_size = 7;        // Sound
-	unsigned char   packet[packet_size];
-
-    unsigned int    incoming_packet_size = 20;
-	unsigned char   incoming_packet[incoming_packet_size] = {};
+    int fd;
+    SerialConfig(&fd);  /* Serial configuration */
 
     printf("\n +----------------------------------+");
     printf("\n |        Kobuki Driver             |");
     printf("\n +----------------------------------+");
-    
-    SerialConfig(&fd);                                          // Serial configuration
-    MountPacket_Twist(packet, packet_size, 0, 0, 5, 0, 0, 0);   // Mount move packet
-    // MountPacket_Sound(packet, packet_size);                     // Mount sound packet
 
-    // printf("\n");    
-    // for (int j = 0; j < sizeof incoming_packet; j++)
-    //         printf("0x%02x ", incoming_packet[j]);
+    int counter = 0;
+    for (;;){   
 
-    for (int i = 0; i < 100; i++){   
- 
-        // written_bytes = write(fd, packet, packet_size);
-        // printf("written %d bytes\n", written_bytes);
-        
-        sleep(3);
-
-        read_bytes = read (fd, incoming_packet, incoming_packet_size);
-        printf("Read %d bytes\n", read_bytes);
-
-        for (int j = 0; j < sizeof incoming_packet; j++)
-            printf("0x%02x ", incoming_packet[j]);
-        
+	read(fd);
+	write(fd);
+       
+        sleep(CFG_DELAY);
+        counter++;
     }
 
     close(fd); /* Close the serial port */
+    return 0;
 
-	return 0;
+}
+
+void read(int fd){
+
+    unsigned char* in_pkt = malloc(sizeof(unsigned char) * IN_BUFFER_SIZE);
+    int read_bytes = read (fd, in_pkt, IN_BUFFER_SIZE);
+
+    printf("Read %d bytes\n", read_bytes);
+
+    for (int i = 0; i < read_bytes; i++)
+        printf("0x%02x ", incoming_packet[i]);
+
+    free(in_pkt);
+}
+
+void write(int* fd){
+    //todo
 
 }
 
 void SerialConfig(int* fd){
 
-    *fd = open("/dev/ttyUSB0",O_WRONLY | O_NOCTTY | O_NONBLOCK | O_NDELAY);	
+    *fd = open("/dev/ttyUSB0",O_WRONLY | O_NOCTTY | O_NONBLOCK | O_NDELAY);
                             
-	if (fd < 0)
+    if (fd < 0)
 		cout << "Error " << errno << " opening " << "/dev/ttyUSB0" << ": " << strerror (errno) << endl;
 
-    struct termios tty;	                        /* Create the structure                          */
-    tcgetattr(*fd, &tty);	                    /* Get the current attributes of the Serial port */
+    struct termios tty;	           /* Create the structure                          */
+    tcgetattr(*fd, &tty);	   /* Get the current attributes of the Serial port */
 
     /* Setting the Baud rate */         
-    cfsetispeed(&tty, B115200);                 /* Set Read  Speed as 9600                       */
-    cfsetospeed(&tty, B115200);                 /* Set Write Speed as 9600                       */
+    cfsetispeed(&tty, B115200);    /* Set Read  Speed as 9600                       */
+    cfsetospeed(&tty, B115200);    /* Set Write Speed as 9600                       */
 
     /* 8N1 Mode */          
-    tty.c_cflag &= ~PARENB;                     /* Disables the Parity Enable bit(PARENB),So No Parity   */
-    tty.c_cflag &= ~CSTOPB;                     /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
-    tty.c_cflag |=  CS8;                        /* Set the data bits = 8                                 */
+    tty.c_cflag &= ~PARENB;        /* Disables the Parity Enable bit(PARENB),So No Parity   */
+    tty.c_cflag &= ~CSTOPB;        /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
+    tty.c_cflag |=  CS8;           /* Set the data bits = 8                                 */
 
-    if((tcsetattr(*fd, TCSANOW, &tty)) != 0)    /* Set the attributes to the termios structure*/
-        printf("\n  ERROR ! in Setting attributes");
-    else
-        printf("\n  BaudRate = 115200 \n  StopBits = 1 \n  Parity   = none");
-        
+    printf(
+        ((tcsetattr(*fd, TCSANOW, &tty)) != 0)
+            ? "\n  ERROR ! in Setting attributes"
+            : "\n  BaudRate = 115200 \n  StopBits = 1 \n  Parity   = none"
+    );
+
     tcflush(*fd, TCIFLUSH);                     /* Discards old data in the rx buffer            */
 }
 
@@ -138,6 +139,7 @@ void MountPacket_Twist( unsigned char* packet,
                         int velocity_z=0, int angle_z=0){
 
     unsigned char packet_checksum = 0;
+    packet = malloc(size * sizeof(unsigned char));
 
     /* PACOTE QUE FAZ MOVER */
     packet[0] = 0xAA; // Header 0
